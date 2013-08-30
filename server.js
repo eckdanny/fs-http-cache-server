7ffbedb1fbfc241c6f136650b02b0c3942a6ce26
Danny Eck <eck.danny@gmail.com>
2013-08-29 21:08:00 -0500

    created pipe

--- server.js
+++ server.js
@@ -4,11 +4,25 @@ var fs   = require('fs')
   ;
 
 var options = {
-  host: "www.google.com",
+  host: 'www.google.com',
   port: 80,
-  path: "/index.html"
+  method: 'GET',
+  path: '/index.html'
 };
 
 http.get(options, function (res) {
+
   console.log('Got response: ' + res.statusCode);
+
+  if (res.statusCode === 200) {
+    res.pipe(fs.createWriteStream('req.json'));
+  }
+
+  // res.setEncoding('utf8');
+  // res.on('data', function (data) {
+  //   console.log('I got some data:', data);
+  // });
+
+}).on('error', function (err) {
+  console.log('[ERROR] ' + err.message);
 });
