15f5819793f03f097217cdcaa60448cc6975fd02
Danny Eck <eck.danny@gmail.com>
2013-08-29 23:26:18 -0500

    just have to handle stale data now

--- server.js
+++ server.js
@@ -1,29 +1,88 @@
 var fs   = require('fs')
-  , path = require('path')
   , http = require('http')
   ;
 
-var options = {
-  hostname:'mobilebyod.shld.net',
-  path: '/stb_dispatcher/service/rest/scantrybuy/getProductDetails?in_upc=884140284212'
-};
 
-http.get(options, function (res) {
+http.createServer(function (req, res) {
 
-  console.log(res.headers);
+  var upc = /^\/upc\/(\d{12})$/.exec(req.url);
 
-  console.log('Got response: ' + res.statusCode);
+  if (upc && upc.length === 2) {
 
-  if (res.statusCode === 200) {
-    res.pipe(fs.createWriteStream('req.json'));
-    res.pipe(fs.createWriteStream('foo.json'));
+    upc = upc.pop();
+
+    fs.exists('cache/'+upc+'.json', function(exists) {
+
+      if (exists) {
+        fs.stat('cache/'+upc+'.json', function (err, stats) {
+
+          // console.log('file last modified: ', stats.mtime);
+
+          var mtime = (new Date(stats.mtime)).getTime();
+
+          // Less than a day old
+          if ( (Date.now() - mtime) < 1000*60*60*24 ) {
+
+            fs.readFile('cache/'+upc+'.json', function (err, data) {
+              if (err) {
+                console.log('[ERROR] Could not read file!');
+              } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(data, 'utf-8');
+              }
+            });
+
+          // The cached response is stale
+          } else {
+
+            // @todo
+            console.log('I have to download a fresh copy!');
+
+          }
+
+        });
+
+      } else {
+
+        var options = {
+          hostname:'mobilebyod.shld.net',
+          path: '/stb_dispatcher/service/rest/scantrybuy/getProductDetails?in_upc=' + upc
+        };
+
+        http.get(options, function (_res_) {
+
+          // console.log(_res_.headers);
+
+          // console.log('Got _response_: ' + _res_.statusCode);
+
+          if (_res_.statusCode === 200) {
+            _res_.pipe(res);
+            _res_.pipe(fs.createWriteStream('cache/'+upc+'.json'));
+          }
+
+          // _res_.setEncoding('utf8');
+          // _res_.on('data', function (data) {
+          //   console.log('I got some data:', data);
+          // });
+
+        }).on('error', function (err) {
+          console.log('[ERROR] ' + err.message);
+        });
+
+      }
+    });
+
+
+
+  } else {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(JSON.stringify({
+      url: req.url,
+      method: req.method,
+      headers: req.headers
+    }));
   }
 
-  // res.setEncoding('utf8');
-  // res.on('data', function (data) {
-  //   console.log('I got some data:', data);
-  // });
+}).listen(4000);
 
-}).on('error', function (err) {
-  console.log('[ERROR] ' + err.message);
-});
