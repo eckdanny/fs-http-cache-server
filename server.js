13d849c80ad20f18d4e2c53188b79ec800d13eca
Danny Eck <eck.danny@gmail.com>
2013-08-29 22:21:41 -0500

    back to working

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ var fs   = require('fs')
   ;
 
 var options = {
-  host: 'www.google.com',
-  port: 80,
-  method: 'GET',
-  path: '/index.html'
+  hostname:'mobilebyod.shld.net',
+  path: '/stb_dispatcher/service/rest/scantrybuy/getProductDetails?in_upc=884140284212'
 };
 
 http.get(options, function (res) {
+
+  console.log(res.headers);
 
   console.log('Got response: ' + res.statusCode);
 
   if (res.statusCode === 200) {
     res.pipe(fs.createWriteStream('req.json'));
+    res.pipe(fs.createWriteStream('foo.json'));
   }
 
   // res.setEncoding('utf8');
