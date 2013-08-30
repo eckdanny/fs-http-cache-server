862de755d9ef750096b2b91687a02bf769f50ce6
Danny Eck <eck.danny@gmail.com>
2013-08-29 20:39:04 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var fs   = require('fs')
+  , path = require('path')
+  , http = require('http')
+  ;
+
+var options = {
+  host: "www.google.com",
+  port: 80,
+  path: "/index.html"
+};
+
+http.get(options, function (res) {
+  console.log('Got response: ' + res.statusCode);
+});
